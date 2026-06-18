e1818ad04536824da13610dab15eab4fba83ddc6
nyxie404 <nishii1610@gmail.com>
2026-06-18 17:25:37 +0530

    main lol

--- hi.cpp
+++ hi.cpp
@@ -1,6 +1,6 @@
 #include<iostream>
 using namespace std;
 int main() {
-    cout << "hiii" << "boooom" << "change in main  lol" << endl;
+    cout << "hiii" << "boooom" << "change in main lol" << endl;
     return 0;
 }
